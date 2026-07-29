76a9d0b41ee820d262a19d0cb40959241670047e
Muhammad Huzaifa <huzaifa031252khan@gmail.com>
2026-07-29 06:31:55 -0700

    correct link

--- components/salesTitle.jsx
+++ components/salesTitle.jsx
@@ -5,7 +5,7 @@ export default function SalesTitle() {
     <nav className="flex w-[100%] justify-center items-center bg-black text-white p-3">
       <h2 className="">
         Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
-        <Link className="ml-2 border-b border-white-500" href="/all-product">
+        <Link className="ml-2 border-b border-white-500" href="/all-products">
           ShopNow
         </Link>
       </h2>
